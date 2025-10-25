ab19c884993d4e1ed057896d373e6f65c316c345
MilFolhas <thor.vieira077@gmail.com>
2025-10-25 19:51:04 -0300

    Iniciando projeto

---
+++ ignt-learn/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FirstComponent from './components/firstComponent'
+function App() {
+
+  return (
+   <div>
+  <FirstComponent/>
+    </div>
+  )
+}
+
+export default App
---
+++ ignt-learn/src/components/FirstComponent.jsx
@@ -0,0 +1,5 @@
+function FirstComponent(){
+    return <div>test</div>
+
+}
+export default FirstComponent
